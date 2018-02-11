3cee229622bda1f51bfceee6b33539372acf85e1
Liam Oliver <Liam Oliver@Oliver-205272.domain_not_set.invalid>
2018-02-11 10:10:13 +0000

    Fib solved

---
+++ Java Problems/src/theFibonacciSeriesProblem/FibonacciMethod.java
@@ -0,0 +1,36 @@
+package theFibonacciSeriesProblem;
+
+import java.util.ArrayList;
+
+public class FibonacciMethod {
+	private static int total;
+	
+	public static int fibSolver(){
+		int fibNum = 1;
+		total = 1;
+		int count = 0;
+		ArrayList<Integer> fibSeq = new ArrayList<Integer>();
+		fibSeq.add(fibNum);
+		while(fibNum < 1000) {
+			if (fibSeq.size() == 1) {
+			}else {
+				int num1 = fibSeq.get(count);
+				int num2 = fibSeq.get(count-1);
+				fibNum = num1 + num2;
+			}
+			count += 1;
+			fibSeq.add(fibNum);
+			total += fibNum;
+		}
+		return (fibNum);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(fibSolver());
+		System.out.println(total);
+		
+	}
+}
